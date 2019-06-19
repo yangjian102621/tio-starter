88a2cbc7d51b7fa30924f877148a4551f67b5a34
yangjian <yangjian102621@gmail.com>
2019-06-20 07:06:37 +0800

    完成初始版本功能

---
+++ demos/spring-boot-starter-demo/src/main/java/org/rockyang/tio/core/server/HelloServerGroupListener.java
@@ -0,0 +1,23 @@
+package org.rockyang.tio.core.server;
+
+import org.rockyang.tio.core.starter.annotation.TioServerGroupListener;
+import org.tio.core.ChannelContext;
+import org.tio.core.intf.GroupListener;
+
+/**
+ * Tio group 监听，通过加 {@link TioServerGroupListener} 注解启用，否则不会启用
+ *
+ * @author yangjian
+ */
+@TioServerGroupListener
+public class HelloServerGroupListener implements GroupListener {
+    @Override
+    public void onAfterBind(ChannelContext channelContext, String s) throws Exception {
+
+    }
+
+    @Override
+    public void onAfterUnbind(ChannelContext channelContext, String s) throws Exception {
+
+    }
+}
---
+++ demos/spring-boot-starter-demo/src/main/java/org/rockyang/tio/core/server/TioServerApplication.java
@@ -0,0 +1,18 @@
+package org.rockyang.tio.core.server;
+
+
+import org.rockyang.tio.core.starter.annotation.EnableTioServerServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yangjian
+ */
+@SpringBootApplication
+@EnableTioServerServer
+public class TioServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TioServerApplication.class, args);
+    }
+}
