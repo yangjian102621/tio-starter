f034c9b794c76e336c24621e1713fc962012bb77
yangjian <yangjian102621@gmail.com>
2019-06-19 22:40:59 +0800

    调整项目结构，初步完成开发

---
+++ tio-spring-boot-autoconfigure/src/main/java/org/rockyang/tio/core/starter/TioMsgHandlerNotFoundException.java
@@ -0,0 +1,10 @@
+package org.rockyang.tio.core.starter;
+
+/**
+ * @author fyp
+ */
+public class TioMsgHandlerNotFoundException extends RuntimeException {
+    public TioMsgHandlerNotFoundException() {
+        super();
+    }
+}
