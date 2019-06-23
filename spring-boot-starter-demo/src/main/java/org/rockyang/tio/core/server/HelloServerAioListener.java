a21c926f02cc8aefe11963a62b2fab488a3dabf2
yangjian <yangjian102621@gmail.com>
2019-06-23 13:19:10 +0800

    调整项目架构，合并到 tio 框架项目中

---
+++ spring-boot-starter-demo/src/main/java/org/rockyang/tio/core/server/HelloServerAioListener.java
@@ -0,0 +1,46 @@
+package org.rockyang.tio.core.server;
+
+import org.tio.core.starter.annotation.TioServerAioListener;
+import org.tio.core.ChannelContext;
+import org.tio.core.intf.Packet;
+import org.tio.server.intf.ServerAioListener;
+
+/**
+ * 消息监听，通过加 {@link TioServerAioListener} 注解启用，否则不会启用
+ *
+ * @author yangjian
+ */
+@TioServerAioListener
+public class HelloServerAioListener implements ServerAioListener {
+
+
+    @Override
+    public void onAfterConnected(ChannelContext channelContext, boolean isConnected, boolean isReconnect) throws Exception {
+
+    }
+
+    @Override
+    public void onAfterDecoded(ChannelContext channelContext, Packet packet, int packetSize) throws Exception {
+
+    }
+
+    @Override
+    public void onAfterReceivedBytes(ChannelContext channelContext, int receivedBytes) throws Exception {
+
+    }
+
+    @Override
+    public void onAfterSent(ChannelContext channelContext, Packet packet, boolean isSentSuccess) throws Exception {
+
+    }
+
+    @Override
+    public void onAfterHandled(ChannelContext channelContext, Packet packet, long cost) throws Exception {
+
+    }
+
+    @Override
+    public void onBeforeClose(ChannelContext channelContext, Throwable throwable, String remark, boolean isRemove) throws Exception {
+
+    }
+}
---
+++ spring-boot-starter-demo/src/main/java/org/rockyang/tio/core/server/controller/HelloController.java
@@ -0,0 +1,36 @@
+package org.rockyang.tio.core.server.controller;
+
+import org.rockyang.tio.core.server.HelloPacket;
+import org.tio.core.starter.TioServerBootstrap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.tio.core.Tio;
+
+/**
+ * @author yangjian
+ */
+@RestController
+public class HelloController {
+
+	@Autowired
+	private TioServerBootstrap bootstrap;
+
+	@GetMapping("/")
+	public String index()
+	{
+		return "Hello, tio-spring-boot-starter !!!";
+	}
+
+	/**
+	 * 推送消息到客户端
+	 * @throws Exception
+	 */
+	@GetMapping("/push")
+	public String pushMessage() throws Exception {
+		HelloPacket packet = new HelloPacket();
+		packet.setBody("This message is pushed by Tio Server.".getBytes(HelloPacket.CHARSET));
+		Tio.sendToAll(bootstrap.getServerGroupContext(), packet);
+		return "Push message to client successfully";
+	}
+}
